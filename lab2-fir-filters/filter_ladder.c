a63670e6e3fb461106a0ff9f3193a47229d7ecb3
kmskillings <kmskillings@gmail.com>
2024-01-31 16:14:21 -0700

    Configured release build.

--- lab2-fir-filters/filter_ladder.c
+++ lab2-fir-filters/filter_ladder.c
@@ -5,9 +5,6 @@
 
 #define SAMPLE_INDEX_MASK (FILTER_LENGTH - 1)
 
-extern float impulse_response[FILTER_LENGTH];   // The impulse response that this filter should have. Defined in lab2.h.
-
-static float    coefficients[FILTER_LENGTH];    // The array of filter coefficients.
 static uint16_t samples[FILTER_LENGTH];         // The array of current and previously-used samples.
 
 static uint16_t sample_index_newest;    // The index of the most recently-taken (newest) sample in samples.
@@ -17,12 +14,10 @@ static uint16_t sample_index_oldest;    // The index of the least recently-taken
 void ladder_initialize_filter() {
 
     // Sets all the samples to 0 (Shouldn't be rigorously necessary, but just to be safe).
-    for (uint16_t i = 0; i < FILTER_LENGTH; i++) {
+    uint16_t i;
+    for (i = 0; i < FILTER_LENGTH; i++) {
         samples[i] = 0;
     }
-
-    // Sets up the filter response.
-    coefficients = impulse_response;
 
 }
 
@@ -43,13 +38,14 @@ void ladder_take_sample(uint16_t sample) {
 // Calculates the next output sample value.
 uint16_t ladder_next_sample() {
 
-    float accumulator   = 0;                    // Accumulates the value of the output sample.
-    float sample_cursor = sample_index_newest;  // The index of the sample that is currently being operated on.
+    float       accumulator   = 0;                    // Accumulates the value of the output sample.
+    uint16_t    sample_cursor = sample_index_newest;  // The index of the sample that is currently being operated on.
 
-    for (uint16_t i = 0; i < FILTER_LENGTH; i++) {
+    uint16_t i;
+    for (i = 0; i < FILTER_LENGTH; i++) {
 
         // Accumulates the next rung of the ladder
-        accumulator = accumulator + samples[sample_cursor] * coefficients[i];
+        accumulator = accumulator + samples[sample_cursor] * impulse_response[i];
 
         // Advances the sample cursor
         sample_cursor = (sample_cursor + 1) & SAMPLE_INDEX_MASK;
