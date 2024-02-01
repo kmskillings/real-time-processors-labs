ffb28cff1a200db81f1eef959e2346130dde3bab
kmskillings <kmskillings@gmail.com>
2024-02-01 12:38:30 -0700

    Lattice filter now meets timing and works correctly.

--- lab2-fir-filters/filter_lattice.c
+++ lab2-fir-filters/filter_lattice.c
@@ -3,10 +3,9 @@
 #include "filter_lattice.h"
 #include "lab2.h"
 
-static float lattice_coefficients[FILTER_LENGTH + 1];
+float lattice_coefficients[FILTER_LENGTH + 1];
 
-static float filter_current_state[FILTER_LENGTH + 1];
-static float filter_previous_state[FILTER_LENGTH + 1];
+float filter_state[FILTER_LENGTH + 1];
 
 static uint16_t output;
 
@@ -29,40 +28,13 @@ void lattice_initialize_filter() {
 // Takes a new sample and advances the filter state.
 void lattice_take_sample(uint16_t sample) {
 
-    // A better way to do things. Marginally more elegant, but uses twice as much memory to store state information.
-    float accumulator;
-
-    filter_previous_state[0] = filter_current_state[0];
-    filter_current_state[0] = sample;
-    accumulator = sample;
-
-    uint16_t i;
-    for (i = 1; i < FILTER_LENGTH + 1; i++) {
-
-        // First, copy out this stage's previous state.
-        filter_previous_state[i] = filter_current_state[i];
-
-        // Calculate this stage's new state.
-        filter_current_state[i] = lattice_coefficients[i] * accumulator + filter_previous_state[i - 1];  // g_i[n] = k_i * f_(i-1) + g_(i-1)[n-1];
-
-        // Calculate the new accumulator value.
-        accumulator = accumulator + lattice_coefficients[i] * filter_previous_state[i - 1];              // f_i[n] = f_(i-1)[n] + k_i * g_(i-1)[n-1];
-    }
-
-    // Now the accumulator should have the value of the output of the lattice filter.
-    // However, since the impulse response has an extraneous 1 at the beginning, we need to subtract
-    // off the initial value again.
-    output = (uint16_t)accumulator - sample;
-
-    // Kind of a dumb, clunky way to do things tbh. But it's the first I thought of.
-    /*
     float accumulator;                  // Progressively adds up the "f" value of the filter.
     float current_stage_last_state;     // The last state of the filter stage currently being calculated.
                                         // Equivalent to g_i[n-1] from the notes.
     float preceding_stage_last_state;   // The last state of the filter stage preceding this one.
                                         // Equivalent to g_(i-1)[n-1] from the notes.
 
-    accumulator = (float)sample;                    // f0[n] = x[n]
+    accumulator = (float)sample;                            // f0[n] = x[n]
     preceding_stage_last_state = filter_state[0];   // Sets up for loop
     filter_state[0] = accumulator;                  // g0[n] = x[n]
 
@@ -87,7 +59,7 @@ void lattice_take_sample(uint16_t sample) {
     // However, since we have an extra 1 at the beginning of the impulse response,
     // we need to subtract the original sample value to get the value to output.
     output = (uint16_t)accumulator - sample;
-    */
+
 
 }
 
@@ -134,8 +106,7 @@ void initialize_state() {
 
     uint16_t i;
     for (i = 0; i < FILTER_LENGTH + 1; i++) {
-        filter_current_state[i] = 0;
-        filter_previous_state[i] = 0;
+        filter_state[i] = 0;
     }
 
 }
