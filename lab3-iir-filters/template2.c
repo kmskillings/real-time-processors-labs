d51e476f7aa9153889456a481af49eb82cc3960f
kmskillings <kmskillings@gmail.com>
2024-02-13 18:09:55 -0700

    Set up CCS project.

---
+++ lab3-iir-filters/template2.c
@@ -0,0 +1,76 @@
+/*
+(C) Copyright Utah State University 2014.  All rights reserved.
+No part of this program may be photocopied, reproduced, or translated
+to another program language without the prior written consent of
+Utah State University.
+
+Time-stamp: " "
+
+(program) template2.c
+(status) Under development
+(description) Template file for output of samples using the OMAP-L138 Experimenter kit.
+(keywords) ECE 5640, Real-Time DSP
+
+(long_d)
+
+This is a template file for use in the lab for ECE 5640.  It is used to provide the
+initialization for the OMAP-L138 Experimenter kit that will send samples to the left
+and right channels.
+
+This template illustrates the use of interrupt service routines for timing.
+
+(long_d)
+
+(see_also) template2.h L138_aic3106_init.c L138_aic3106_init.h vectors_intr.asm
+
+(bugs)
+There are no known bugs at this time
+(bugs)
+
+(author) Andrew Pound
+
+(modifications)
+Written 04/01/13 by Andrew Pound
+Modified: See subversion logs.
+(modifications)
+
+
+  Info: $Id: template2.c 755 2014-02-04 22:08:39Z scott $   USU
+  Revision: $Rev: 755 $
+ $LastChangedDate: 2014-02-04 15:08:39 -0700 (Tue, 04 Feb 2014) $
+ $LastChangedBy: scott $
+
+*/
+
+#include "L138_aic3106_init.h"
+
+// This data structure allows for outputting to both channels.
+AIC31_data_type codec_data;
+
+interrupt void interrupt4(void)
+{
+  float yn,xn = 0.0;
+  
+  // Get the next sample of the input.
+  yn = (float)(input_left_sample()); // input from ADC
+  xn = (float)(input_right_sample()); // input from ADC
+
+// Insert processing code here......
+  
+  // output to BOTH right and left channels...
+  codec_data.channel[LEFT] = (uint16_t)(yn);
+  codec_data.channel[RIGHT] = (uint16_t)(xn);
+  output_sample(codec_data.uint);  // output to L and R DAC
+
+  return;
+}
+
+int main(void)
+{
+// Filter initialization code goes here....
+
+  L138_initialise_intr(FS_48000_HZ,ADC_GAIN_0DB,DAC_ATTEN_0DB);
+
+  while(1);
+
+}
